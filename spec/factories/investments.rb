f952b14201418cb3ebfdfbcdee255227b74bba57
amite <amit.erandole@gmail.com>
2014-10-14 20:04:51 +0530

    added missing attributes to factory

--- spec/factories/investments.rb
+++ spec/factories/investments.rb
@@ -2,6 +2,13 @@ require "../spec_helper"
 
 FactoryGirl.define do
   factory :investment do
+    name 'Zenith Birla'
     initial_deposit 3_00_000
+    type 'Quarterly Interest'
+    cumulative false
+    folio_no 52357
+    rate_of_interest 11.25
+    created_at 25/10/2014
+    term '3 years'
   end
 end
